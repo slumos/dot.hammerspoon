d2e3e758c8ce0d968b704cc8737f683bdaf47370
Steven Lumos <steven@lumos.us>
2024-02-19 16:57:14 -0800

    remove workaround we probably don't need anymore

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-# Oh yay, Hammerspoon moved to Lua 5.4 before Homebrew packaged it? So do this now to use the ASDF Lua.
-package.path = "/Users/slumos/.asdf/installs/lua/5.4.0/share/lua/5.4/?.lua;/Users/slumos/.asdf/installs/lua/5.4.0/share/lua/5.4/?/init.lua;/Users/slumos/.asdf/installs/lua/5.4.0/luarocks/share/lua/5.4/?.lua;/Users/slumos/.asdf/installs/lua/5.4.0/luarocks/share/lua/5.4/?/init.lua;"..package.path
-package.cpath = "/Users/slumos/.asdf/installs/lua/5.4.0/lib/lua/5.4/?.so;/Users/slumos/.asdf/installs/lua/5.4.0/luarocks/lib/lua/5.4/?.so;"..package.cpath
-
 local fennel = require "fennel"
 table.insert(package.loaders or package.searchers, fennel.searcher)
 fennel.dofile("init.fnl", {allowedGlobals = false})
